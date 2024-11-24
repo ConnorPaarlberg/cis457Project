7b70b709cd220c5b0d44626e6c07e2f02121e9f0
Andrew Hua <andrewhua08@gmail.com>
2024-11-24 00:35:35 -0500

    Error Checking and Bug Fixes

--- Battleship.py
+++ Battleship.py
@@ -24,6 +24,16 @@ class Square:
 
     def add_ship_to_square(self,Square_SHIP):
         self.ship = Square_SHIP
+    
+    def square_attacked(self):
+        if self.ship != Square_SHIP.NOTHING:
+            self.state = Square_State.HIT
+            print("You hit a ship!")
+            return True
+        else:
+            self.state = Square_State.MISS
+            print("You missed!")
+            return False
 
 class Board:
     def __init__(self):
@@ -33,6 +43,8 @@ class Board:
         self.battlefield = [[Square() for x in range(10)] for y in range(10)]
         # self.player_name = 
 
+        self.number_of_ships_placed = 0
+
         # Health points for each of the ships on the board
         # If 0, they're dead
         self.carrier_hp = 5
@@ -41,49 +53,73 @@ class Board:
         self.submarine_hp = 3
         self.destroyer_hp = 2
 
-        self.number_of_ships_placed = 0
-
     def check_dead_board(self):
         sum = self.carrier_hp + self.battleship_hp + self.cruiser_hp + self.submarine_hp + self.destroyer_hp
         if sum == 0:
             return True
         return False
     
+    def decrement_ship_health(self, hurt_ship):
+        match hurt_ship:
+            case Square_SHIP.CARRIER:
+                self.carrier_hp-=1
+            case Square_SHIP.BATTLESHIP:
+                self.battleship_hp-=1
+            case Square_SHIP.CRUISER:
+                self.cruiser_hp -=1
+            case Square_SHIP.SUBMARINE:
+                self.submarine_hp -=1
+            case Square_SHIP.DESTROYER:
+                self.destroyer_hp -=1
+        if self.check_dead_board() == True:
+            print("This board is now dead")
+            self.state = Board_State.DEAD
+    
     # Location is a tuple for the x and y axis [x,y]
     # SQUARE_SHIP is what ship we're placing
     # ship_length is how big the ship is
     # vertical_placement_bool - 0 if horizonal placement, 1 if vertical placement
+    # Returns true if it was able to place a ship properly
     def place_ship_on_board(self, location, ship, ship_length, vertical_placement_bool):
         Invalid_Input = False
+        ship_available = True
+        row = location[0]
+        column = location[1]
 
-        Xaxis = location[0]
-        Yaxis = location[1]
-
-        if Xaxis < 0 or Xaxis > 9 or Yaxis < 0 or Yaxis > 9:
+        if row < 0 or row > 9 or column < 0 or column > 9:
             Invalid_Input = True
 
-        if Xaxis + ship_length > 9 or Yaxis + ship_length > 9:
-            Invalid_Input = True
+        if vertical_placement_bool:
+            if row + ship_length > 10:
+                Invalid_Input = True
+        else:
+            if column + ship_length > 10:
+                Invalid_Input = True
 
         if not Invalid_Input:
-            ship_available = True
             if vertical_placement_bool:
                 for i in range(ship_length):
-                    if self.battlefield[Xaxis+i][Yaxis].ship != Square_SHIP.NOTHING:
+                    if self.battlefield[row+i][column].ship != Square_SHIP.NOTHING:
                         ship_available = False
                 if ship_available:
                     for i in range(ship_length):
-                        self.battlefield[Xaxis+i][Yaxis].ship = ship
+                        self.battlefield[row+i][column].ship = ship
                         self.number_of_ships_placed += 1 
 
             else:
                 for i in range(ship_length):
-                    if self.battlefield[Xaxis][Yaxis+i].ship != Square_SHIP.NOTHING:
+                    if self.battlefield[row][column+i].ship != Square_SHIP.NOTHING:
                         ship_available = False
                 if ship_available:
                     for i in range(ship_length):
-                        self.battlefield[Xaxis][Yaxis+i].ship = ship
-                        self.number_of_ships_placed += 1      
+                        self.battlefield[row][column+i].ship = ship
+                        self.number_of_ships_placed += 1
+        if Invalid_Input or not ship_available:
+            print(Invalid_Input)
+            print(ship_available)
+            return False
+        return True
+         
 
     def print_board_state(self):
         print_list = []
@@ -105,8 +141,19 @@ class Board:
 
     def print_board_ships(self):
         print_list = []
+        print_column_list = ["  "]
+        row_counter = 0
+        column_counter = 0
+
+        for i in range(10):
+            print_column_list.append(str(column_counter))
+            print_column_list.append("  ")
+            column_counter+=1
+        print("".join(print_column_list))
+
 
         for i in range (10):
+            print_list.append(str(row_counter))
             for j in range (10):
                 print_list.append("|")
                 current_square_state = self.battlefield[i][j].ship
@@ -123,7 +170,8 @@ class Board:
                     print_list.append("SU")
                 else:
                     print_list.append("DE")
-                
+            row_counter +=1
             print_list.append("|")
             print_list.append("\n")
         print("".join(print_list))
+
--- Game.py
+++ Game.py
@@ -1,29 +1,73 @@
 from Battleship import *
 
-def convert_coordinate_input(input):
-    coordinates = input.split(',')
-    return [int(coordinates[0]), int(coordinates[1])]
+def get_coordinate_input(message):
+    if len(message) > 0:
+        print(message)
 
+    coordinates = input().split(' ')
+    if len(coordinates) != 2:
+        print("Invalid Input")
+        get_coordinate_input(message)
+
+    try:
+        xaxis = int(coordinates[0])
+        yaxis = int(coordinates[1])
+    except Exception:
+        print("Invalid Input")
+
+    else:
+        if(xaxis > 9 or xaxis < 0 or yaxis > 9 or yaxis < 0):
+            print("Coordinates not on board")
+            get_coordinate_input(message)
+        else:
+            return [xaxis,yaxis]
+    
+def get_vertical_bool():
+    print("Are you placing it vertically? (y/n)")
+    vertical_bool = input()
+    if vertical_bool == 'y':
+        return True
+    else: return False
+
+def build_board(board,ship,length,message):
+    location = get_coordinate_input(message)
+
+    while board.place_ship_on_board(location, ship, length, get_vertical_bool()) == False:
+        print("Invalid Placement, try again!")
+        location = get_coordinate_input("")
+
+    board.print_board_ships()
+ 
+def attack_board(target_board):
+    #TODO Check for valid location
+    coordinates = get_coordinate_input("Where should we attack?")
+    xaxis = coordinates[0]
+    yaxis = coordinates[1]
+    target_square = target_board.battlefield[xaxis][yaxis]
+
+    if target_square.state == Square_State.NOT_TOUCHED:
+        target_square.square_attacked()
+        target_board.decrement_ship_health(target_square.ship)
+
+    else:
+        print("This square has already been revealed!")
 
 Current_Board = Board()
 
-print("Where should the carrier go?")
-carrier_location = convert_coordinate_input(input())
-Current_Board.place_ship_on_board(carrier_location,Square_SHIP.CARRIER, 5, bool(input("Are you placing it vertically? (True/False) ")))
-
-
-
-print("Where should the battleship go?")
-battleship_location = convert_coordinate_input(input())
-Current_Board.place_ship_on_board(carrier_location,Square_SHIP.BATTLESHIP, 5, bool(input("Are you placing it vertically? (True/False) ")))
-print("Where should the cruiser go?")
-cruiser_location = convert_coordinate_input(input())
-Current_Board.place_ship_on_board(carrier_location,Square_SHIP.CRUISER, 5, bool(input("Are you placing it vertically? (True/False) ")))
-print("Where should the submarine go?")
-submarine_location = convert_coordinate_input(input())
-Current_Board.place_ship_on_board(carrier_location,Square_SHIP.SUBMARINE, 5, bool(input("Are you placing it vertically? (True/False) ")))
-print("Where should the destroyer go?")
-destroyer_location = convert_coordinate_input(input())
-Current_Board.place_ship_on_board(carrier_location,Square_SHIP.DESTROYER, 5, bool(input("Are you placing it vertically? (True/False) ")))
-
 Current_Board.print_board_ships()
+
+build_board(Current_Board, Square_SHIP.CARRIER, 5, "Where should the carrier go?")
+
+build_board(Current_Board, Square_SHIP.BATTLESHIP, 4, "Where should the battleship go?")
+
+build_board(Current_Board, Square_SHIP.CRUISER, 3, "Where should the cruiser go?")
+
+build_board(Current_Board, Square_SHIP.SUBMARINE, 3, "Where should the submarine go?")
+
+build_board(Current_Board, Square_SHIP.DESTROYER, 2, "Where should the destroyer go?")
+
+
+while Current_Board.state == Board_State.ALIVE: 
+    attack_board(Current_Board)
+    Current_Board.print_board_ships()
+    Current_Board.print_board_state()
